e502936b8c9a5dc7e5a94daffa0a24cfc16985e5
Lewis Daly <lewis@vesselstech.com>
2021-07-06 11:49:11 +0930

    feat: add initial ci config

--- scripts/_deploy.sh
+++ scripts/_deploy.sh
@@ -10,5 +10,5 @@ echo "Deploying Version: v${VERSION}"
 docker-compose build
 
 # re-tag the image docker-compose built for us
-docker tag ldaly/dev-portal:latest ldaly/dev-portal:v${VERSION}
-docker push ldaly/dev-portal:v${VERSION}
+docker tag mojaloop/contrib-dev-portal:latest mojaloop/contrib-dev-portal:v${VERSION}
+docker push ojaloop/contrib-dev-portal:v${VERSION}
