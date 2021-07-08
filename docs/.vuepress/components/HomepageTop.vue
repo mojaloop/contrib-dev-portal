17be038bd0cd01d29d6d3f2c2215b031b9c9ca29
Lewis Daly <lewis@vesselstech.com>
2021-07-08 10:25:02 +0930

    feat: styling of top stuff

--- docs/.vuepress/components/HomepageTop.vue
+++ docs/.vuepress/components/HomepageTop.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <div class="columns box block">
+  <div class=block>
+    <div class="columns">
       <div class="column">
         <!-- <button @click="testAlert">{{ displayText }}</button> -->
         <h3 class="title is-spaced">Payments</h3>
@@ -13,38 +13,38 @@
           </b-button>
       </div>
       <div class="column">
-        <img src="/header-code.png" />
+        <img class="header-image" src="/header-code.png" />
       </div>
     </div>
+    <div class="custom-divider"></div>
 
     <div class="columns">
       <div class="column">
-        <img class="box" src="business_ops.png" />
-        <h3>Business Operations</h3>
-        <p>
+        <img class="" src="business_ops.png" />
+        <h3 class="title is-4">Business Operations</h3>
+        <p class="subtitle is-5">
           Explore how Hub Operators interact with Mojaloop APIs to onboard DFSPs
           and PISPs
         </p>
-          <b-button type="is-primary is-light" icon-right="arrow-right">
-            Learn More
-          </b-button>        
-          <h4>IN THIS SECTION</h4>
-        <ul>
-          <li><a href="/">Onboarding new participants to a Mojaloop Hub</a></li>
-          <li><a href="/">Running Settlement operations</a></li>
-        </ul>
+        <b-button class="block" type="is-primary is-light" icon-right="arrow-right">
+          Learn More
+        </b-button>        
+        <h4 class="subtitle is-6">IN THIS SECTION</h4>
+        <a href="/">- Onboarding new participants to a Mojaloop Hub</a>
+        <br/>
+        <a href="/">- Running Settlement operations</a>
       </div>
       <div class="column">
-        <img class="box" src="als.png" />
-        <h3>Account Lookup</h3>
-        <p>
-          See how DFSPs use the Account Lookup Service to register identifiers
+        <img class="" src="als.png" />
+        <h3 class="title is-4">Account Lookup</h3>
+        <p class="subtitle is-5">
+           See how DFSPs use the Account Lookup Service to register identifiers
           with Mojaloop
         </p>
-          <b-button type="is-primary is-light" icon-right="arrow-right">
-            Learn More
-          </b-button>          
-          <h4>IN THIS SECTION</h4>
+        <b-button class="block" type="is-primary is-light" icon-right="arrow-right">
+          Learn More
+        </b-button>        
+        <h4 class="subtitle is-6">IN THIS SECTION</h4>
         <ul>
           <li><a href="/">Register an identifier for a user</a></li>
           <li><a href="/">Look up a user from an identifier</a></li>
