4df74f86e64edb8c30581eba8b5b63e328d83cdf
Lewis Daly <lewis@vesselstech.com>
2021-08-04 14:23:01 +0930

    feat: update the 3p scheme adapter sync api

--- scripts/_update_external_assets.sh
+++ scripts/_update_external_assets.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# TODO: this should be master once the following PR is merged: 
+# https://github.com/mojaloop/thirdparty-scheme-adapter/pull/110
 
+BRANCH=feat/2365-update-outbound-interface
 # Updates any external assets we have copied into this repo.
 
 # TODO: we should really get this from api-snippets...
-wget https://raw.githubusercontent.com/mojaloop/thirdparty-scheme-adapter/master/src/interface/api-outbound.yaml\
-  -O ${DIR}/../docs/apis/3p-scheme-adapter-outbound.yaml
+wget https://raw.githubusercontent.com/mojaloop/thirdparty-scheme-adapter/${BRANCH}/src/interface/api-outbound.yaml\
+  -O ${DIR}/../docs/.vuepress/public/3p-scheme-adapter-outbound.yaml
 
 # TODO: modify the default responses for easier reading - this is currently a manual process
