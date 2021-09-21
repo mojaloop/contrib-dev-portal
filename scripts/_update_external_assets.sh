649a12e7d1cec959efd72a84fd377279b6e730cc
Lewis Daly <lewis@vesselstech.com>
2021-09-21 13:06:52 +0930

    feat: update external assets

--- scripts/_update_external_assets.sh
+++ scripts/_update_external_assets.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-# TODO: this should be master once the following PR is merged: 
-# https://github.com/mojaloop/thirdparty-scheme-adapter/pull/110
 
-BRANCH=feat/2365-update-outbound-interface
+
 # Updates any external assets we have copied into this repo.
+
+BRANCH=master
 
 # TODO: we should really get this from api-snippets...
 wget https://raw.githubusercontent.com/mojaloop/thirdparty-scheme-adapter/${BRANCH}/src/interface/api-outbound.yaml\
   -O ${DIR}/../docs/.vuepress/public/3p-scheme-adapter-outbound.yaml
 
-# TODO: modify the default responses for easier reading - this is currently a manual process
+
+
+echo 'Copied across external assets. Now make sure you do the following manually:'
+echo ' - modify the default responses for easier reading - this is currently a manual process'
